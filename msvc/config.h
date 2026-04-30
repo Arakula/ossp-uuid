bf0554f5ed7c55fe533100748c73018f36c2b944
Hermann Seib <office@hermannseib.com>
2026-04-30 17:18:15 +0200

    Little correction for MSVC < 2013

--- msvc/config.h
+++ msvc/config.h
@@ -232,4 +232,4 @@
 
 /* Define to id of used va_copy() implementation */
 /* #define __VA_COPY_USE __VA_COPY_USE_C99 */
-#define __VA_COPY_USE __VA_COPY_USE_CPP
+#define __VA_COPY_USE __VA_COPY_USE_CPS
